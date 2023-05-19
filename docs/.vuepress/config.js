ec20c8d9b0905555655288e888c87e94710284bd
dyzhwork <dyzhwork@gmail.com>
2023-05-19 17:27:09 +0800

    配置顶部导航栏二级菜单

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,14 @@ module.exports = {
     logo: "https://img2.baidu.com/it/u=2770443583,649975410&fm=253&fmt=auto&app=138&f=JPEG?w=600&h=399",
     nav: [
       { text: "首页", link: "/" },
+      {
+        text: "前端相关",
+        items: [
+          { text: "HTML5", link: "/html/" },
+          { text: "CSS3", link: "/css/" },
+          { text: "JavaScript", link: "/javascript/" },
+        ],
+      },
       { text: "指南", link: "/guide/" },
       { text: "VuePress", link: "/vuepress/" },
       { text: "谷歌", link: "https://google.com" },
