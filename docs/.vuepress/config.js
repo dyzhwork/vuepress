3fa764cb8e784c324942ff1815afda8f0dc82c5b
dyzhwork <dyzhwork@gmail.com>
2023-05-19 17:16:28 +0800

    配置顶部导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
   title: "你好世界", // 顶部左侧标题
   description: "VuePress相关文档学习", // html 文件描述
+  themeConfig: {
+    logo: "https://img2.baidu.com/it/u=2770443583,649975410&fm=253&fmt=auto&app=138&f=JPEG?w=600&h=399",
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "指南", link: "/guide/" },
+      { text: "谷歌", link: "https://google.com" },
+    ],
+  },
 };
