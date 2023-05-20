880b23aefa387e78bb1c06fce6d5405586f6e18e
dyzhwork <dyzhwork@gmail.com>
2023-05-20 12:00:12 +0800

    配置顶部导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,1 +1,28 @@
 // 配置文件的入口文件
+module.exports = {
+  theme: "vdoing",
+  title: "你好啊", // 顶部左侧标题
+  description: "VuePress相关文档学习", // html 文件描述
+  themeConfig: {
+    logo: "image/panda.png",
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "前端相关",
+        items: [
+          { text: "纯HTML", items: [{ text: "HTML5", link: "/html/" }] },
+          { text: "纯CSS", items: [{ text: "CSS3", link: "/css/" }] },
+          {
+            text: "JavaScript",
+            items: [{ text: "JavaScript", link: "/javascript/" }],
+          },
+        ],
+      },
+      { text: "谷歌", link: "https://google.com" },
+    ],
+    // 侧边栏
+    sidebar: {
+      "/html/": [],
+    },
+  },
+};
