3b2153cfc342e32e5aaf99515284b651421cea94
dyzhwork <dyzhwork@gmail.com>
2023-05-21 16:47:16 +0800

    配置浏览器标签title旁边icon图片

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,29 @@
 // 配置文件的入口文件
 module.exports = {
-  theme: "vdoing",
-  title: "你好啊", // 顶部左侧标题
-  description: "VuePress相关文档学习", // html 文件描述
-  themeConfig: {
-    logo: "image/panda.png",
-    nav: [
-      { text: "首页", link: "/" },
-      {
-        text: "前端相关",
-        items: [
-          { text: "纯HTML", items: [{ text: "HTML5", link: "/html/" }] },
-          { text: "纯CSS", items: [{ text: "CSS3", link: "/css/" }] },
-          {
-            text: "JavaScript",
-            items: [{ text: "JavaScript", link: "/javascript/" }],
-          },
-        ],
-      },
-      { text: "谷歌", link: "https://google.com" },
-    ],
-    // 侧边栏
-    sidebar: {
-      "/html/": [],
-    },
-  },
+	theme: "vdoing",
+	title: "你好啊", // 顶部左侧标题
+	description: "VuePress相关文档学习", // html 文件描述
+	head: [["link", { rel: "icon", href: "/image/panda.png" }]],
+	themeConfig: {
+		logo: "image/panda.png",
+		nav: [
+			{ text: "首页", link: "/" },
+			{
+				text: "前端相关",
+				items: [
+					{ text: "纯HTML", items: [{ text: "HTML5", link: "/html/" }] },
+					{ text: "纯CSS", items: [{ text: "CSS3", link: "/css/" }] },
+					{
+						text: "JavaScript",
+						items: [{ text: "JavaScript", link: "/javascript/" }],
+					},
+				],
+			},
+			{ text: "谷歌", link: "https://google.com" },
+		],
+		// 侧边栏
+		sidebar: {
+			"/html/": [],
+		},
+	},
 };
