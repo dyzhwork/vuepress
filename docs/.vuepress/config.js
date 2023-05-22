a6b2bac592886bbd94826de2586163af29bfaae9
dyzhwork <dyzhwork@gmail.com>
2023-05-22 11:49:50 +0800

    代码格式化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,75 +1,75 @@
 // 配置文件的入口文件
 module.exports = {
-	theme: "vdoing",
-	title: "你好啊", // 顶部左侧标题
-	description: "VuePress相关文档学习", // html 文件描述
-	head: [["link", { rel: "icon", href: "/image/panda.png" }]],
-	themeConfig: {
-		logo: "/image/panda.png",
-		nav: [
-			{ text: "首页", link: "/" },
-			{
-				text: "前端相关",
-				items: [
-					{ text: "纯HTML", items: [{ text: "HTML5", link: "/html/" }] },
-					{ text: "纯CSS", items: [{ text: "CSS3", link: "/css/" }] },
-					{
-						text: "JavaScript",
-						items: [
-							{ text: "JavaScript", link: "/javascript/" },
-							{ text: "Canvas", link: "/canvas/" },
-							{ text: "Echarts", link: "/echarts/" },
-						],
-					},
-				],
-			},
-			{
-				text: "后端相关",
-				items: [
-					{
-						text: "NodeJS",
-						items: [
-							{ text: "原生nodeJS", link: "/node/" },
-							{ text: "Express框架", link: "/express/" },
-							{ text: "Sequelize框架", link: "/sequelize/" },
-						],
-					},
-					{ text: "API", items: [{ text: "apifox", link: "/api/" }] },
-				],
-			},
-			{
-				text: "Git相关",
-				items: [
-					{ text: "git下载安装配置", link: "/node/" },
-					{ text: "git可视化", link: "/express/" },
-					{ text: "git报错", link: "/sequelize/" },
-				],
-			},
-			{ text: "报错整理", link: "/deal-with-error/" },
-			{ text: "谷歌", link: "https://google.com" },
-		],
-		// 侧边栏 有四种写法,数组,数组对象,对象数组,自动生成
-		sidebar: {
-			"/html": [
-				{
-					title: "标签元素",
-					sidebarDepath: 2,
-					children: [
-						{ title: "基本元素", path: "/html/basic-element" },
-						{ title: "常用元素", path: "/html/common-element" },
-						{ title: "h5新增元素", path: "/html/h5-add-element" },
-					],
-				},
-			],
-			"/canvas": [
-				{
-					title: "二维特效",
-					sidebarDepath: 2,
-					children: [
-						{ title: "小球碰撞回弹效果", path: "/canvas/pelletcollision" },
-					],
-				},
-			],
-		},
-	},
+  theme: "vdoing",
+  title: "你好啊", // 顶部左侧标题
+  description: "VuePress相关文档学习", // html 文件描述
+  head: [["link", { rel: "icon", href: "/image/panda.png" }]],
+  themeConfig: {
+    logo: "/image/panda.png",
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "前端相关",
+        items: [
+          { text: "纯HTML", items: [{ text: "HTML5", link: "/html/" }] },
+          { text: "纯CSS", items: [{ text: "CSS3", link: "/css/" }] },
+          {
+            text: "JavaScript",
+            items: [
+              { text: "JavaScript", link: "/javascript/" },
+              { text: "Canvas", link: "/canvas/" },
+              { text: "Echarts", link: "/echarts/" },
+            ],
+          },
+        ],
+      },
+      {
+        text: "后端相关",
+        items: [
+          {
+            text: "NodeJS",
+            items: [
+              { text: "原生nodeJS", link: "/node/" },
+              { text: "Express框架", link: "/express/" },
+              { text: "Sequelize框架", link: "/sequelize/" },
+            ],
+          },
+          { text: "API", items: [{ text: "apifox", link: "/api/" }] },
+        ],
+      },
+      {
+        text: "Git相关",
+        items: [
+          { text: "git下载安装配置", link: "/node/" },
+          { text: "git可视化", link: "/express/" },
+          { text: "git报错", link: "/sequelize/" },
+        ],
+      },
+      { text: "报错整理", link: "/deal-with-error/" },
+      { text: "谷歌", link: "https://google.com" },
+    ],
+    // 侧边栏 有四种写法,数组,数组对象,对象数组,自动生成
+    sidebar: {
+      "/html": [
+        {
+          title: "标签元素",
+          sidebarDepath: 2,
+          children: [
+            { title: "基本元素", path: "/html/basic-element" },
+            { title: "常用元素", path: "/html/common-element" },
+            { title: "h5新增元素", path: "/html/h5-add-element" },
+          ],
+        },
+      ],
+      "/canvas": [
+        {
+          title: "二维特效",
+          sidebarDepath: 2,
+          children: [
+            { title: "小球碰撞回弹效果", path: "/canvas/pelletcollision" },
+          ],
+        },
+      ],
+    },
+  },
 };
