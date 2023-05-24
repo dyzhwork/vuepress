6b8ae4836c05d503560456a7eccf738251591f66
dyzhwork <dyzhwork@gmail.com>
2023-05-24 09:26:11 +0800

    顶部菜单,侧边栏菜单配置文件整理

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+// menu内容主要针对导航栏和侧边栏菜单进行相关配置
+const menu = require('./menu.config.js')
 // 配置文件的入口文件
 module.exports = {
   theme: 'vdoing',
@@ -13,94 +15,19 @@ module.exports = {
     logo: '/image/panda.png',
     nav: [
       { text: '首页', link: '/' },
-      {
-        text: '前端相关',
-        items: [
-          { text: '纯HTML', items: [{ text: 'HTML5', link: '/html/' }] },
-          { text: '纯CSS', items: [{ text: 'CSS3', link: '/css/' }] },
-          {
-            text: 'JavaScript',
-            items: [
-              { text: 'JavaScript', link: '/javascript/' },
-              { text: 'Canvas', link: '/canvas/' },
-              { text: 'Echarts', link: '/echarts/' }
-            ]
-          }
-        ]
-      },
-      {
-        text: '后端相关',
-        items: [
-          {
-            text: 'NodeJS',
-            items: [
-              { text: '原生nodeJS', link: '/node/' },
-              { text: 'Express框架', link: '/express/' },
-              { text: 'Sequelize框架', link: '/sequelize/' }
-            ]
-          },
-          { text: 'API', items: [{ text: 'apifox', link: '/api/' }] }
-        ]
-      },
-      {
-        text: 'Git相关',
-        items: [
-          { text: 'Git下载安装', link: '/git/git-install' },
-          { text: 'Git有必要配置', link: '/git/git-config' },
-          { text: 'Git的可视化', link: '/git/git-visualization' },
-          { text: 'Git相关报错', link: '/git/git-handle-error' }
-        ]
-      },
+      { text: '前端相关', items: menu.navBarlist.front_end_correlation },
+      { text: '后端相关', items: menu.navBarlist.back_end_correlation },
+      { text: 'Git相关', items: menu.navBarlist.git_correlation },
       { text: '报错整理', link: '/deal-with-error/' },
       { text: '谷歌', link: 'https://google.com' }
     ],
     // 侧边栏 有四种写法,数组,数组对象,对象数组,自动生成
     sidebar: {
-      '/html': [
-        {
-          title: '标签元素',
-          sidebarDepath: 2,
-          children: [
-            { title: '一、基本元素', path: '/html/basic-element' },
-            { title: '二、常用元素', path: '/html/common-element' },
-            { title: '三、h5新增元素', path: '/html/h5-add-element' }
-          ]
-        }
-      ],
-      '/css': [
-        {
-          title: '装扮标签元素',
-          sidebarDepath: 2,
-          children: [
-            { title: '一、CSS语法', path: '/css/css-syntactic' },
-            { title: '二、CSS选择器', path: '/css/css-selector' }
-          ]
-        }
-      ],
-      '/javascript': [
-        {
-          title: '操作标签元素',
-          sidebarDepath: 2,
-          children: [{ title: '原生事件', path: '/javascript/origin-event' }]
-        }
-      ],
-      '/canvas': [
-        {
-          title: 'canvas绘图API',
-          sidebarDepath: 2,
-          children: [
-            { title: '小球碰撞回弹效果', path: '/canvas/pelletcollision' },
-            { title: '两个小球连接运动效果', path: '/canvas/kinematicconnection' }
-          ]
-        }
-      ],
-      '/echarts': [
-        {
-          title: 'Echarts数据可视化',
-          sidebarDepath: 2,
-          children: [{ title: '柱状图', path: '/echarts/chart-bar' }]
-        }
-      ]
+      '/html': menu.sideBarList.html,
+      '/css': menu.sideBarList.css,
+      '/javascript': menu.sideBarList.javascript,
+      '/canvas': menu.sideBarList.canvas,
+      '/echarts': menu.sideBarList.echarts
     }
   }
 }
